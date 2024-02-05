36600a282cdfdb63cae5e88a2940fb971dda291e
andrejakobsson7 <andre.jakobsson7@hotmail.com>
2024-02-05 20:45:44 +0100

    Add pages for adding new superhero and index

---
+++ SuperheroSocialClub/Pages/Superhero/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SuperheroSocialClub.Pages.Superhero
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
